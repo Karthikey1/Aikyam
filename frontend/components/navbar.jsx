c928ce3fa7981fa7b8811ed35c8860b9a37454b6
nischith_r <nrnischith2004@gmail.com>
2025-03-06 23:23:59 +0530

    routing+++

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -150,7 +150,7 @@ export default function PersistentDrawerRight({ changePage }) {
             {
               text: "Map",
               icon: <AddLocationAltIcon />,
-              path: "/map",
+              path: "/maps",
             },
             {
               text: "Market Place",
